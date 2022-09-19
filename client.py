7394ac0ce7266067508d6989d671bdd410e0e208
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-09-19 10:48:25 +0900

    Create client.py

---
+++ client.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+
+Class Listener:
+   def __init__(self, name, port, ipaddress):
+        
+        self.name       = name
+        self.port       = port
+        self.ipaddress  = ipaddress
+
+if __name__ == "__main__":
+    main()
+    
